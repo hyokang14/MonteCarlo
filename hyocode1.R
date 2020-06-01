908eadf3b45b7bf66ca7f023a8ebde0646f4b3e4
hyokang14 <65795493+hyokang14@users.noreply.github.com>
2020-06-01 17:31:15 -0500

    Update hyocode1.R

--- hyocode1.R
+++ hyocode1.R
@@ -39,6 +39,49 @@ N<-ceiling(N)
 
 
 
+#Accept Rejection method 
+
+efficiency<-1/0.40622
+N2<-ceiling((n/efficiency)*1.1)
+
+#Accept Rejection method 
+
+efficiency<-1/0.40622
+N2<-ceiling((n/efficiency)*1.1)
+
+#under is the code of accept and rejection code from professor please ignore
+#library(pracma)
+
+#n<-10^3
+#efficiency<-64/135 #efficiency of the method
+#N<-ceiling(n/efficiency*1.1)
+#Y<-runif(N) #uniform random numbers as candidate numbers
+#W<-runif(N) #uniform random numbers used in criterion 
+#Z<-numeric(n)
+
+#tic()
+#flag<-1 #check whether we get enough sample for Z
+#i<-1
+#while (flag<=n){
+#  if (W[i]<=efficiency*20*Y[i]*(1-Y[i])^3) {
+#    Z[flag]<-Y[i]
+#    flag<-flag+1
+#  }
+#  i<-i+1
+#}
+#toc()
+#hist(Z,freq=F,ylim=c(0,2.5))
+#xfit<-seq(min(Z),max(Z),length=1000)
+#lines(xfit, 20*xfit*(1-xfit)^3,col='blue',lwd=2)
+
+#no loop
+#tic()
+#Z<-Y[which(efficiency*20*Y*(1-Y)^3>=W)]
+#Z<-Z[1:n]
+#toc()
+#hist(Z,freq=F,ylim=c(0,2.5))
+#xfit<-seq(min(Z),max(Z),length=1000)
+#lines(xfit, 20*xfit*(1-xfit)^3,col='blue',lwd=2)
 
 
 
